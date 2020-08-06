d249e77b787ecd062594f85aacaad0bfa3769970
The Art Appreciator Art <pvtfreestone1@gmail.com>
2020-08-06 13:40:48 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+var= x + 1;
+while (x <= 20){
+    if (x / 3)= true {
+    console.log("Julia");
+    }
+    if (x/ 5)= true {
+        console.log("James");
+    }
+    if (x/3) && (x/5) {
+console.log("JuliaJames");
+    }
+    else{
+        console.log('Prints x ' + x);
+        x= x + 1;
+    }
